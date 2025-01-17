f84f96c4a72953a0e64bfaf9d6d219e06836db9d
linlin <sdsantoscarla@gmail.com>
2025-01-17 15:36:25 -0300

    grafico de pizza com os alimentos todos juntos num só gráfico

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -206,7 +206,7 @@ function somar_calorias() {
         },
     });
 
-    var graficosPizza = $('#graficos-pizza');
+    /*var graficosPizza = $('#graficos-pizza');
     graficosPizza.empty();
 
     for (var alimento in gramas) {
@@ -271,4 +271,39 @@ function somar_calorias() {
               });
         }
     }
+}*/
+new Chart(document.getElementById('graficos-pizza').getContext('2d'), {
+    type: 'pie',
+    data: {
+        labels: labels,
+        datasets: [{
+            data: data,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        title: {
+            display: true,
+            text: 'Distribuição de Calorias por Alimento'
+        }
+    }
+});
 }
